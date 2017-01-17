5e289e1a6c8871aebf12bda74733f7e2e00ec632
adcreare <caveman@commscentral.net>
2017-01-17 17:34:41 -0500

    wip create record in CF

--- lib/dreamflare/CF_api_calls.rb
+++ lib/dreamflare/CF_api_calls.rb
@@ -13,6 +13,37 @@ module Dreamflare
             SecureRandom.uuid
         end
 
+        def update_record(name,type,value)
+
+            uri  = URI("https://api.cloudflare.com/client/v4/zones/af53c6784ad906452f9b8ed589fd805b/dns_records")
+            puts uri
+
+            https = Net::HTTP.new(uri.host,uri.port)
+            https.use_ssl = true
+
+            req = Net::HTTP::Post.new(uri.path)
+            req.add_field('Content-Type', 'application/json')
+            req['X-Auth-Key'] = @@APIKey
+            req['X-Auth-Email'] = @@APIEmail
+            req["body"] = {'type' => type,'name' => name,'content' => value}.to_json
+
+            puts req["body"]
+
+
+
+            res = https.request(req)
+
+            puts(JSON.parse(res.body))
+
+            throw "DONE"
+
+            # curl -X POST "https://api.cloudflare.com/client/v4/zones/023e105f4ecef8ad9ca31a8372d0c353/dns_records" \
+            #      -H "X-Auth-Email: user@example.com" \
+            #      -H "X-Auth-Key: c2547eb745079dac9320b638f5e225cf483cc5cfdda41" \
+            #      -H "Content-Type: application/json" \
+            #      --data '{"type":"A","name":"example.com","content":"127.0.0.1","ttl":120,"proxied":false}'
+
+        end
 
         def query(zone,command, params = '')
 
