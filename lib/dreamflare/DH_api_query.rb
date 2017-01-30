29a10f41ae95eee30512ecb84740c17aac230d25
adcreare <caveman@commscentral.net>
2017-01-29 20:00:37 -0500

    object formating code now in class

--- lib/dreamflare/DH_api_query.rb
+++ lib/dreamflare/DH_api_query.rb
@@ -20,11 +20,67 @@ module Dreamflare
             connection.use_ssl = true
             data = connection.get(uri.request_uri)
 
-            return JSON.parse(data.body)
+            responseObject = processResponse(JSON.parse(data.body))
+
+            return responseObject
 
         end
 
-    end
+
+        private
+
+        def processResponse(dhResults)
+
+          matchingDHZones = []
+
+          #if the result was a success from DH then for each record returned
+          if dhResults['result'] == 'success'
+              dhResults['data'].each do |dnsRecord| #
+
+                  if(dnsRecord['zone'] == SearchZone) #See if it mathces our search zone
+
+                      #If the value of the record has a space in it - its probably got a priority set
+                      # go and cut that out if possible
+                      if(dnsRecord['value'].count(" ") > 0)
+                          resultOfSplit = dnsRecord['value'].split
+
+                          # If weh have more than two spaces in the record we probably don't have a priority set
+                          # so update as a normal record
+                          if(resultOfSplit.length > 2)
+                              h = {'record' => dnsRecord['record'],'value' => dnsRecord['value'].chomp('.'),'type' => dnsRecord['type']}
+                          else
+
+                              # Try and get an int out of the first char, if we can't and we still got this far there is something odd about this record
+                              # Throw and error an move on
+                              begin
+                                priority = Integer(resultOfSplit[0])
+                                h = {'record' => dnsRecord['record'],'value' => resultOfSplit[1].chomp('.'),'type' => dnsRecord['type'], 'priority' => Integer(resultOfSplit[0]) }
+                              rescue
+                                puts ('unable to cast to interger the front value of the dns record from dreamhost: '+dnsRecord['value'])
+                              end
+
+                          end
+
+                      # Standard record add it to the matching records
+                      else
+                          h = {'record' => dnsRecord['record'],'value' => dnsRecord['value'].chomp('.'),'type' => dnsRecord['type']}
+                      end
+
+                      # Push into matching zones as long as something got set into h above, otherwise skip and keep going
+                      unless(h.nil?)
+                        matchingDHZones.push(h)
+                      end
+
+
+                  end
+              end
+          end
+
+          return matchingDHZones
+
+        end #end processResponse
+
+    end #end DHAPIQuery
 
     ##################
     #
@@ -38,4 +94,4 @@ module Dreamflare
 
     end
 
-end
+end #end module
